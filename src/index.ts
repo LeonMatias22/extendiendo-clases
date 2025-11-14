e48128667f10d6b1022d63254b9593cc131955cd
Matias Leon <matiasleon.br.2010@gmail.com>
2025-11-14 20:36:31 -0300

    feat: implementar removeProduct y getSortedByPrice en ListaDeProductos

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import products from "./products.json";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +26,49 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(n: string) {
+    // Llamada al constructor de la superclase: LisaDeCosas
+    super(n);
+
+    // Lógica adicional para leer products.json y agregar productos usando addProduct
+    // ...
+    products.forEach((product) => {
+      this.addProduct(product);
+    })
+
+    // Ejemplo de cómo se puede invocar addProduct para agregar un producto
+    // this.addProduct(new Product(/* parámetros del producto */));
+  }
+
+  addProduct(product: Product): void {
+    const existe = this.cosas.find(producto => producto.id === product.id);
+    if(!existe){
+      this.cosas.push(product)
+    }
+  }
+
+  getProduct(id: number): Product {
+    const result = this.cosas.find(producto => producto.id === id);
+    return result
+  }
+
+  removeProduct(id: number): void {
+    this.cosas = this.cosas.filter(producto => producto.id !== id)
+  }
+
+  getSortedByPrice(order: string): Product[] {
+  switch(order) {
+    case "asc":
+      return this.cosas.sort((a, b) => a.price - b.price); // Orden ascendente
+    case "desc":
+      return this.cosas.sort((a, b) => b.price - a.price); // Orden descendente
+    default:
+      return this.cosas; // Si no es "asc" ni "desc", devuelve el array sin cambios
+  }
+}
+
+}
+
 
 export { ListaDeProductos, Product };
